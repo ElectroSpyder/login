b55bca8cd5f8ac4ac39f887b1e115f7a20ff14ef
Pedro Rene Gonzalez <prg@live.com.ar>
2018-04-18 14:59:40 -0300

    Primero se ejecuta en consola de paquetes nugets Add-Migration InitialCreate sobre el proyecto ModeloDeDatos, aqui tambien hay que agregar por nuget referencia a Microsoft.EntityFrameworkCore.SqlServer,  Microsoft.EntityFrameworkCore.Tools y Microsoft.VisualStudio.Web.CodeGeneration.Design luego por ultimo Update-Database

---
+++ Net.Core.LoginSolucion/Net.Core.LoginSolucion.Web.ModeloDeDatos/Migrations/20180418174109_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Net.Core.LoginSolucion.Api.ModeloDeDatos.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Apellido = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Estado = table.Column<bool>(nullable: false),
+                    Nombre = table.Column<string>(nullable: true),
+                    NombreUsuario = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
